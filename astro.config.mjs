bb899012f076b4b70e30ba6a9a1996d4798b1bd5
Iskander <iskander.sierra@gmail.com>
2024-08-22 11:39:23 +0200

    astro add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+  site: 'https://isierraprofile.netlify.app/',
+  integrations: [mdx(), sitemap(), react()],
+  output: "server",
+  adapter: netlify()
 });
